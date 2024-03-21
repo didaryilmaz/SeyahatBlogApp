bccd45508483d21204ec1a4868bd4c4a6408a266
Selfdeveloop <91085429+didaryilmaz@users.noreply.github.com>
2024-03-21 23:52:28 +0300

    first page update

---
+++ adventravelApp/src/screens/DetailScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const DetailsScreen = () => {
+return <SafeAreaView> </SafeAreaView>;
+};
+const style = StyleSheet.create({});
+
+export default DetailsScreen;
---
+++ adventravelApp/src/screens/HomeScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+const HomeScreen = ({navigation}) => {
+return <SafeAreaView></SafeAreaView>;
+};
+const style = StyleSheet.create({});
+export default HomeScreen;
