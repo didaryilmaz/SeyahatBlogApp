842387492641dbf401ed5ae16182f1de02d138b1
Selfdeveloop <91085429+didaryilmaz@users.noreply.github.com>
2024-03-30 15:17:28 +0300

    update adventravel

--- adventravelApp/App.js
+++ adventravelApp/App.js
@@ -1,97 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import StartPage from './src/components/StartPage';
+import { 
+  StyleSheet, 
+  View, 
+} from 'react-native';
+import LoginPage from './src/components/LoginPage';
 
 const App = () =>{
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = () => {
-  };
-  const handleForgotPassword = () => {
-  };
-  const handleCreateAccount = () => {
-  };
-
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Şifre"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <View style={styles.rememberMe}>
-        <Checkbox
-          value={rememberMe}
-          onValueChange={setRememberMe}
-        />
-        <Text style={styles.rememberMeText}>Beni Hatırla</Text>
-      </View>
-      <Button title="Giriş" onPress={handleLogin} />
-      <View style={styles.forgotPassword}>
-        <Text style={styles.forgotPasswordText} onPress={handleForgotPassword}>
-          Şifreni mi Unuttun?
-        </Text>
-      </View>
-      <View style={styles.createAccount}>
-        <Text style={styles.createAccountText} onPress={handleCreateAccount}>
-          Yeni Hesap Oluştur
-        </Text>
-      </View>
+      <LoginPage/>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    width: 300,
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  },
-  rememberMe: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  rememberMeText: {
-    fontSize: 12,
-    color: '#888',
-  },
-  forgotPassword: {
-    fontSize: 12,
-    color: '#888',
-    marginTop: 10,
-  },
-  forgotPasswordText: {
-    color: "#000",
-    textDecorationLine: "underline",
-  },
-  createAccount: {
-    fontSize: 12,
-    color: '#888',
-    marginTop: 10,
-  },
-  createAccountText: {
-    color: "#000",
-    textDecorationLine: "underline",
+    flex: 1
   }
-});
+})
 
 export default  App;
---
+++ adventravelApp/src/components/LoginPage.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  TextInput, 
+  View, 
+  Button 
+} from 'react-native';
+
+const LoginPage = () =>{
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+  };
+  const handleForgotPassword = () => {
+  };
+  const handleCreateAccount = () => {
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Şifre"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <View style={styles.rememberMe}>
+        <Checkbox
+          value={rememberMe}
+          onValueChange={setRememberMe}
+        />
+        <Text style={styles.rememberMeText}>Beni Hatırla</Text>
+      </View>
+      <Button title="Giriş" onPress={handleLogin} />
+      <View style={styles.forgotPassword}>
+        <Text style={styles.forgotPasswordText} onPress={handleForgotPassword}>
+          Şifreni mi Unuttun?
+        </Text>
+      </View>
+      <View style={styles.createAccount}>
+        <Text style={styles.createAccountText} onPress={handleCreateAccount}>
+          Yeni Hesap Oluştur
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  rememberMe: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  rememberMeText: {
+    fontSize: 12,
+    color: '#888',
+  },
+  forgotPassword: {
+    fontSize: 12,
+    color: '#888',
+    marginTop: 10,
+  },
+  forgotPasswordText: {
+    color: "#000",
+    textDecorationLine: "underline",
+  },
+  createAccount: {
+    fontSize: 12,
+    color: '#888',
+    marginTop: 10,
+  },
+  createAccountText: {
+    color: "#000",
+    textDecorationLine: "underline",
+  }
+});
+
+export default  LoginPage;
